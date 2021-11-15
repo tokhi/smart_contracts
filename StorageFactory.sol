9a219bd2007639b22dceb37c7e2dc000b12a4262
Shafi Tokhi <shafi.tokhi@aroundhome.de>
2021-11-15 14:30:28 +0100

    simple storage factory

---
+++ StorageFactory.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.6.0;
+
+import "./SimpleStorage.sol";
+
+contract SotrageFactory{
+    
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorage() public {
+        SimpleStorage simplestorage = new SimpleStorage();
+        simpleStorageArray.push(simplestorage);
+    }
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //address
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+    }
+}
